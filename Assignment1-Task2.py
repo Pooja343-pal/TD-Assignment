0c2135a49056b6ae0749133447916a2dfca868f4
Pooja343-pal <pal.0709@gmail.com>
2025-07-08 22:04:21 +0530

    Add files via upload

---
+++ Assignment1-Task2.py
@@ -0,0 +1,10 @@
+ #Assignment1 -Task2
+ #Step 1: Take user's first and last name as input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Step 2: Concatenate the first name and last name
+full_name = first_name + " " + last_name
+
+# Step 3: Print a personalized greeting
+print(f"Hello, {full_name}! Welcome aboard.")
