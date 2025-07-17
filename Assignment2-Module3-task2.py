8c33160b8803b6ef1e7fd68f56710ed60ee3f3b6
Pooja343-pal <pal.0709@gmail.com>
2025-07-17 12:43:13 +0530

    Add files via upload

---
+++ Assignment2-Module3-task2.py
@@ -0,0 +1,9 @@
+#  Initialize the sum variable
+total_sum = 0
+
+# Loop  from 1 to 50
+for num in range(1, 51):
+    total_sum += num  # Add each number to total_sum
+
+#  Display the final sum
+print(f"The sum of integers from 1 to 50 is: {total_sum}")
